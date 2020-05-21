c54c8388551ce86a24c77ee94bf7d0522fedd498
jpettett <joseph.pettett@gmail.com>
2020-05-21 11:55:44 -0700

    dynamic search bar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import {
+  searchBox,
+  SearchBox,
+} from './components/searchbox/searchbox.component';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +11,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -17,9 +22,18 @@ class App extends Component {
   }
 
   render() {
+    // filters monsters in search field
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
